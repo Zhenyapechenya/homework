fe73318aaf7e73d00619243c8468433340fbeff5
Zhenyapechenya <epsareva77@gmail.com>
2023-11-17 00:05:28 +0700

    done authentification

--- 3.3-permissions/api_with_restrictions/advertisements/models.py
+++ 3.3-permissions/api_with_restrictions/advertisements/models.py
@@ -22,9 +22,5 @@ class Advertisement(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,15 +1,27 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwnerOrReadonly
+from advertisements.serializers import AdvertisementSerializer
+
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadonly]
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+        # return super().perform_create(serializer)
 
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsOwnerOrReadonly()]
         return []

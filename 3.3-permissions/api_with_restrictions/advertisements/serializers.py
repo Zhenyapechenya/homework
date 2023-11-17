bc8c4f2e55325e851d771cecf3687da3c98c48d9
Zhenyapechenya <epsareva77@gmail.com>
2023-11-17 21:18:51 +0700

    made validation for 10 opened advertisements

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -27,19 +27,21 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """Метод для создания"""
-
+        
         # Простановка значения поля создатель по-умолчанию.
         # Текущий пользователь является создателем объявления
         # изменить или переопределить его через API нельзя.
         # обратите внимание на `context` – он выставляется автоматически
         # через методы ViewSet.
         # само поле при этом объявляется как `read_only=True`
-        validated_data["creator"] = self.context["request"].user
+        validated_data['creator'] = self.context['request'].user
         return super().create(validated_data)
 
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
 
-        # TODO: добавьте требуемую валидацию
-
+        user = self.context['request'].user
+        print(Advertisement.objects.filter(creator=user, status='OPEN').count())
+        if Advertisement.objects.filter(creator=user, status='OPEN').count() >= 10:
+            raise serializers.ValidationError("У вас уже есть 10 открытых объявлений.")
         return data

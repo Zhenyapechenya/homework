be2f9b80785e61b575d23653626e7b7020b39ad9
Zhenyapechenya <epsareva77@gmail.com>
2023-11-20 20:45:47 +0700

    add rights for admin (delete and change)

--- 3.3-permissions/api_with_restrictions/advertisements/permissions.py
+++ 3.3-permissions/api_with_restrictions/advertisements/permissions.py
@@ -6,6 +6,10 @@ class IsOwnerOrReadonly(BasePermission):
         # Разрешить GET, HEAD или OPTIONS запросы (т.е. только для чтения).
         if request.method in ('GET'):
             return True
+        
+        # Проверка, является ли пользователь администратором.
+        if request.user.is_staff:
+            return True
 
         # Проверка, является ли пользователь владельцем объявления.
         return request.user == obj.creator

93dcd401e7123a6ac5da119e676a1dbb312dc58b
Zhenyapechenya <epsareva77@gmail.com>
2023-11-23 15:04:34 +0700

    try to test random things

--- 3.4-django-testing/django_testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/django_testing/settings.py
@@ -83,6 +83,8 @@ DATABASES = {
         'NAME': 'netology_django_testing',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
     }
 }
 
@@ -115,7 +117,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+# USE_L10N = True
 
 USE_TZ = True
 
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,27 @@
+import pytest
+from rest_framework.test import APIClient
+
+from students.models import Course, Student
+
+
 def test_example():
-    assert False, "Just test example"
+    assert True, "Just test example"
+
+
+@pytest.mark.django_db
+def test_api():
+    # Arrange
+    client = APIClient()
+    student = Student.objects.create(name='Sblushin')
+    course = Course.objects.create(name='Mathematics')
+    course.students.add(student)
+
+    # Act
+    response = client.get('/api/v1/courses/')
+
+    # Assert
+    assert response.status_code == 200
+
+    data = response.json()
+    assert len(data) == 1
+    assert data[0]['name'] == 'Mathematics'
